95c20fa89a7dde4495e481978b086d5f2ad200ff
Manuel Argüelles <manuel.arguelles@gmail.com>
2015-07-13 23:33:30 -0500

    Remove unused code
    Removes commented code.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Roidsbuster
-A super silly game for my 3 years old kid
+A super silly game for my 3 year old son
 
 Requires LÖVE (https://love2d.org/)
 
+## Running from source
+
+cd into it and execute love .
--- main.lua
+++ main.lua
@@ -3,23 +3,6 @@ debug = true
 local spaceship = require('spaceship')
 local asteroids = require('asteroids')
 
--- Laser
-canShoot = true
-canShootTimerMax = 0.2
-canShootTimer = canShootTimerMax
-
-bulletImg = nil
-
-bullets = {}
-
--- Enemis
-createEnemyTimerMax = 0.4
-createEnemyTimer = createEnemyTimerMax
-
-enemyImg = nil
-
-enemies = {}
-
 function love.load(arg)
    spaceship.load(arg)
    asteroids.load(arg)
@@ -48,27 +31,7 @@ function love.update(dt)
    spaceship.move_lasers(dt)
 
    asteroids.create(dt)
-
    asteroids.move(dt)
-
-   -- enemies
---   createEnemyTimer = createEnemyTimer - (1 * dt)
---   if createEnemyTimer < 0 then
---      createEnemyTimer = createEnemyTimerMax
-
-      -- create an enemy
---      randomNumber = math.random(10, love.graphics.getWidth() - 10)
---      newEnemy = { x = randomNumber, y = -100, img = enemyImg }
---      table.insert(enemies, newEnemy)
---   end
-
---   for i, enemy in ipairs(enemies) do
---      enemy.y = enemy.y + (200 * dt)
---      if enemy.y > 850 then
---	 table.remove(enemies, i)
---	 spaceship.decrease_score()
---      end
---   end
 
    local lasers_table = spaceship.get_lasers()
    for i, laser in ipairs(lasers_table) do
@@ -86,45 +49,10 @@ function love.update(dt)
       spaceship.killed()
    end
 
---  for i, enemy in ipairs(enemies) do
---     for j, bullet in ipairs(bullets) do
---	 if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(),
---			   enemy.img:getHeight(), bullet.x, bullet.y,
---			   bullet.img:getWidth(), bullet.img:getHeight()) then
---	    table.remove(bullets, j)
---	    table.remove(enemies, i)
---	    spaceship.increase_score()
---	 end
---     end
---
---     if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(),
---			enemy.img:getHeight(), spaceship.x, spaceship.y,
---			spaceship.img:getWidth(), spaceship.img:getHeight()) and
---     isAlive then
---	 table.remove(enemies, i)
---	 isAlive = false
---     end
---  end
---
    if love.keyboard.isDown('r') then
       spaceship.revive()
       asteroids.reset()
    end
-
---     -- remove everything
---     bullets = {}
---     enemies = {}
---
---     -- reset timers
---     canShootTimer = canShootTimerMax
---     createEnemyTimer = createEnemyTimerMax
---
---     spaceship.x = 200
---     spaceship.y = 710
---
---     score = 0
---     spaceship.alive = true
---  end
 
 end
 
@@ -132,22 +60,5 @@ end
 function love.draw(dt)
    spaceship.draw(dt)
    asteroids.draw(dt)
---   for i, bullet in ipairs(bullets) do
---      love.graphics.draw(bullet.img, bullet.x, bullet.y)
---   end
-
---   for i, enemy in ipairs(enemies) do
---      love.graphics.draw(enemy.img, enemy.x, enemy.y)
---   end
 end
 
--- Collision detection taken function from http://love2d.org/wiki/BoundingBox.lua
--- Returns true if two boxes overlap, false if they don't
--- x1,y1 are the left-top coords of the first box, while w1,h1 are its width and height
--- x2,y2,w2 & h2 are the same, but for the second box
-function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
-   return x1 < x2+w2 and
-      x2 < x1+w1 and
-      y1 < y2+h2 and
-      y2 < y1+h1
-end
